5752a60a7c449d71d55e559aa04e5ef639d2df08
zeroxbt <original.swazi@protonmail.com>
2026-02-21 14:13:29 +0500

    Use package version in HTTP info route

--- src/controllers/http_api_controller/v1/info.rs
+++ src/controllers/http_api_controller/v1/info.rs
@@ -3,9 +3,10 @@ use axum::Json;
 use crate::controllers::http_api_controller::v1::dto::info::InfoResponse;
 
 pub(crate) struct InfoHttpApiController;
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 impl InfoHttpApiController {
     pub(crate) async fn handle_request() -> Json<InfoResponse> {
-        Json(InfoResponse::new("6.0.12"))
+        Json(InfoResponse::new(APP_VERSION))
     }
 }
