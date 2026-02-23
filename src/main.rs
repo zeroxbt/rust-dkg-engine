ac6323a444adb918f11e246cb2272f5858225877
zeroxbt <original.swazi@protonmail.com>
2026-02-23 10:12:22 +0500

    Add optional jemalloc build and split release workflows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "jemalloc")]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 #[tokio::main]
 async fn main() {
     rust_dkg_engine::run().await;
